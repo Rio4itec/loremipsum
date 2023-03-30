dde0909573214fe64a208fa61df5539f25d12765
¨Daniel <tec24@outlook.com>
2023-03-30 16:12:12 -0300

    Agregamos contar.sh a la rama

---
+++ contar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo  loremipsum-1.txt tiene $(wc -l <loremipsum-1.txt) lineas
+echo  loremipsum-2.txt tiene $(wc -l <loremipsum-2.txt) lineas
+echo  loremipsum-3.txt tiene $(wc -l <loremipsum-3.txt) lineas
+echo  loremipsum-4.txt tiene $(wc -l <loremipsum-4.txt) lineas
+echo  loremipsum-5.txt tiene $(wc -l <loremipsum-5.txt) lineas
+
+
